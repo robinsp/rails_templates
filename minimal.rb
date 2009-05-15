bc030c3c7ceac81846c036483ae46980e156bd17
Robin Spainhour <robin@robinspainhour.com>
2009-05-15 09:10:12 +0200

    Initial import

---
+++ minimal.rb
@@ -0,0 +1,7 @@
+# = Minimal Rails app
+# I use this to set up a basic Rails application root used by rspec-rails specs
+# during gem development.
+
+run "rm Rakefile README"
+run "rm -rf app/helpers doc log public script test test "
+
